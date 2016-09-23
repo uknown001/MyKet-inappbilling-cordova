481824d6822ffee7b99906a3558f8da7532bdc58
Farzad Seid Arabi <farzad.seid.arabi@gmail.com>
2016-09-23 13:22:11 +0330

    cafebazzar

--- src/android/com/smartmobilesoftware/util/IabHelper.java
+++ src/android/com/smartmobilesoftware/util/IabHelper.java
@@ -254,8 +254,8 @@ public class IabHelper {
             }
         };
 
-        Intent serviceIntent = new Intent("com.android.vending.billing.InAppBillingService.BIND");
-        serviceIntent.setPackage("com.android.vending");
+        Intent serviceIntent = new Intent("ir.cafebazaar.pardakht.InAppBillingService.BIND");
+        serviceIntent.setPackage("com.farsitel.bazaar");
         List service = mContext.getPackageManager().queryIntentServices(serviceIntent, 0);
         if (service != null && !service.isEmpty()) {
             // service available to handle that Intent
